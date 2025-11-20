a8406be2780703485871b698891cbff5c7d16f5a
Daniel Escobar <daanielescobar@gmail.com>
2025-11-20 15:54:52 -0600

    make spinner full page

--- frontend/components/ui/spinner.tsx
+++ frontend/components/ui/spinner.tsx
@@ -4,12 +4,14 @@ import { cn } from "@/lib/utils"
 
 function Spinner({ className, ...props }: React.ComponentProps<"svg">) {
   return (
-    <LoaderIcon
-      role="status"
-      aria-label="Loading"
-      className={cn("size-4 animate-spin", className)}
-      {...props}
-    />
+    <div className="flex justify-center items-center h-full">
+      <LoaderIcon
+        role="status"
+        aria-label="Loading"
+        className={cn("size-4 animate-spin", className)}
+        {...props}
+      />
+    </div>
   )
 }
 
