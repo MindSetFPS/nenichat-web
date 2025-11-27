bd560dc2f428205a86ac357a2a63bd5fc3ecdbe6
Daniel Escobar <daanielescobar@gmail.com>
2025-11-27 17:55:36 -0600

    send messages

--- frontend/Nenichat/Messages/app/send-message.ts
+++ frontend/Nenichat/Messages/app/send-message.ts
@@ -1,4 +1,8 @@
-export default async function SendMessage(phone: string, message: string) {
+import { contactRepository } from "@/Nenichat/Contacts/infra/persistance/ContactRepository";
+import { messageRepository } from "@/Nenichat/Messages/infra/persistance/MessageRepository";
+import { Message } from "../domain/Message";
+
+export default async function SendMessage(phone: string, messageTextContent: string) {
     const res = await fetch("http://192.168.1.64:5100/send/message", {
         method: "POST",
         headers: {
@@ -6,9 +10,30 @@ export default async function SendMessage(phone: string, message: string) {
         },
         body: JSON.stringify({
             phone: phone,
-            message: message,
+            message: messageTextContent,
         }),
     });
 
-    console.log(await res.json())
+    // find the user contact
+    const me = await contactRepository.findMe();
+
+    // find the contact to send the message to
+    const contact = await contactRepository.findByPhoneNumber(phone); // chat_id is contact.id
+    let response = await res.json()
+
+    // Save incoming message
+    if (response.code === "SUCCESS") {
+        const message: Message = new Message(
+            response.results.message_id,
+            contact!.id!,
+            me!.id!,
+            messageTextContent,
+            undefined,
+            undefined,
+            new Date(),
+        )
+        await messageRepository.save(message)
+    } else {
+        console.error("Failed to send message", response)
+    }
 }
