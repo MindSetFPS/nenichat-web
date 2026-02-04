f6cce11bde66151051be73cae3850132290c03ad
Daniel Escobar <daanielescobar@gmail.com>
2026-02-03 21:19:15 -0600

    move message api

--- app/(app)/api/messages/route.ts
+++ app/(app)/api/messages/route.ts
@@ -1,5 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { messageRepository } from '@/Nenichat/Messages/infra/persistance/MessageRepository';
+import SendMessage from '@/Nenichat/Messages/app/send-message';
 
 export async function GET(request: NextRequest) {
   const { searchParams } = new URL(request.url);
@@ -16,4 +17,27 @@ export async function GET(request: NextRequest) {
     total,
     totalPages: Math.ceil(total / pageSize),
   });
+}
+
+
+/**
+ * API route to send a message.
+ * @param request 
+ * @returns 
+ */
+export async function POST(request: NextRequest) {
+  try {
+    const { phone, message } = await request.json();
+
+    if (!phone || !message) {
+      return NextResponse.json({ error: 'Missing phone or message' }, { status: 400 });
+    }
+
+    await SendMessage(phone, message);
+
+    return NextResponse.json({ success: true });
+  } catch (error) {
+    console.error('Error in send message API:', error);
+    return NextResponse.json({ error: 'Failed to send message' }, { status: 500 });
+  }
 }
