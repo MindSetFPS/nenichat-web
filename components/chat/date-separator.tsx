568ef466546b65d9640ea2ed48925019a3a42696
Daniel Escobar <daanielescobar@gmail.com>
2026-04-12 16:33:01 -0600

    string format

--- components/chat/date-separator.tsx
+++ components/chat/date-separator.tsx
@@ -2,7 +2,7 @@ import { IMessage } from "@/Nenichat/Messages/domain/IMessage";
 
 export default function DateSeparator({ messages, index }: { messages: IMessage[], index: number }) {
     return (
-        <div className="sticky top-0 z-10 bg-muted w-min mx-auto px-2 py-1 mb-1 rounded border-muted shadow-lg">
+        <div className="sticky top-0 z-10 bg-muted w-fit mx-auto px-2 py-1 mb-1 rounded border-muted">
             <p
                 className="text-xs text-muted-foreground "
                 key={index}
@@ -17,13 +17,19 @@ export default function DateSeparator({ messages, index }: { messages: IMessage[
                         d1.getFullYear() === d2.getFullYear() &&
                         d1.getMonth() === d2.getMonth() &&
                         d1.getDate() === d2.getDate();
+                    const isSameYear = (d1: Date, d2: Date) => d1.getFullYear() === d2.getFullYear();
 
                     if (isSameDay(messageDate, today)) {
                         return "Hoy";
                     } else if (isSameDay(messageDate, yesterday)) {
                         return "Ayer";
                     } else {
-                        return messageDate.toLocaleDateString('en-GB'); // dd/mm/yyyy format
+                        const dayName = messageDate.toLocaleDateString('es-ES', { weekday: 'long' });
+                        const capitalizedDayName = dayName.charAt(0).toUpperCase() + dayName.slice(1);
+                        const dayMonth = messageDate.toLocaleDateString('es-ES', { day: 'numeric', month: 'long' });
+                        const year = !isSameYear(messageDate, today) ? ` ${messageDate.getFullYear()}` : '';
+                        
+                        return `${capitalizedDayName} ${dayMonth}${year}`;
                     }
                 })()}
             </p>
