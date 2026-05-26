2e5d94e842f036205598f548f898d3328e2b6645
Daniel Escobar <daanielescobar@gmail.com>
2026-05-26 17:39:42 -0600

    add nav bar

--- app/signup/layout.tsx
+++ app/signup/layout.tsx
@@ -1,5 +1,7 @@
 import { ThemeProvider } from "@/components/theme-provider"
 import "@/styles/globals.css"
+import Navigation from "@/components/marketing/navigation"
+import { ToasterProvider } from "@/components/toaster-provider"
 import { GalleryVerticalEnd } from "lucide-react"
 
 interface AuthLayoutProps {
@@ -17,6 +19,7 @@ export default function AuthLayout({ children }: AuthLayoutProps) {
           enableSystem
           disableTransitionOnChange
         >
+          <Navigation />
           <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
             <div className="flex w-full max-w-sm flex-col gap-6">
               <a href="#" className="flex items-center gap-2 self-center font-medium">
@@ -28,6 +31,7 @@ export default function AuthLayout({ children }: AuthLayoutProps) {
               {children}
             </div>
           </div>
+          <ToasterProvider />
         </ThemeProvider>
       </body>
     </html>
